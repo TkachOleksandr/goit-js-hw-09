f57d08c2d650b78ddc242ab31ee40d1cc6b52305
TkachOleksandr <sashatkach66@gmail.com>
2025-01-18 15:34:27 +0000

    Deploying to gh-pages from @ TkachOleksandr/goit-js-hw-09@4114c5022a3811248e47a9945e8756d3b8ddc66e 🚀

---
+++ 1-gallery.js
@@ -0,0 +1,10 @@
+import"./assets/modulepreload-polyfill-B5Qt9EMX.js";import{S as r}from"./assets/vendor-CgTBfC_f.js";const t=document.querySelector(".gallery"),i=data.map(({preview:a,original:l,description:e})=>`<li class="gallery-item hvr-grow">
+    <a class="gallery-link " href="${l}">
+      <img
+        class="gallery-image"
+        src="${a}"
+        alt="${e}"
+      />
+    </a>
+  </li>`);t.insertAdjacentHTML("beforeend",i.join(""));new r(".gallery a",{captionsData:"alt",captionDelay:250});
+//# sourceMappingURL=1-gallery.js.map
