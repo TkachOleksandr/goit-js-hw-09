8eebfb708c6f109f5420e3ec3a7841f09887cb9e
TkachOleksandr <sashatkach66@gmail.com>
2025-01-18 19:09:16 +0000

    Deploying to gh-pages from @ TkachOleksandr/goit-js-hw-09@4a7b98065886cdedd53baf9e69129f509aaa1b38 🚀

--- 1-gallery.js
+++ 1-gallery.js
@@ -1,10 +1,2 @@
-import"./assets/modulepreload-polyfill-B5Qt9EMX.js";import{S as r}from"./assets/vendor-CgTBfC_f.js";const t=document.querySelector(".gallery"),i=data.map(({preview:a,original:l,description:e})=>`<li class="gallery-item hvr-grow">
-    <a class="gallery-link " href="${l}">
-      <img
-        class="gallery-image"
-        src="${a}"
-        alt="${e}"
-      />
-    </a>
-  </li>`).join("");t.insertAdjacentHTML("beforeend",i.join(""));new r(".gallery a",{captionsData:"alt",captionDelay:250});
+import"./assets/modulepreload-polyfill-B5Qt9EMX.js";import{S as t}from"./assets/vendor-CgTBfC_f.js";const e=document.querySelector(".gallery");e.addEventListener("click",onClickOpenImg);e.insertAdjacentHTML("beforeend",imgItmes.join(""));new t(".gallery a",{captionsData:"alt",captionDelay:250});
 //# sourceMappingURL=1-gallery.js.map
